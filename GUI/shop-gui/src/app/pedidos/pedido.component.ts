84db15535a9d0318fb9a15fed3eaa43c72c6a901
felipebma <32962329+felipebma@users.noreply.github.com>
2019-06-22 04:37:40 -0300

    Update pedido.component.ts

--- GUI/shop-gui/src/app/pedidos/pedido.component.ts
+++ GUI/shop-gui/src/app/pedidos/pedido.component.ts
@@ -3,10 +3,7 @@ import { NgModule } from '@angular/core'
 
 import { Produto } from "../produto"
 import { ProdutoService } from "../produto.service"
-<<<<<<< HEAD
 import { PedidoService } from '../pedido.service';
-=======
->>>>>>> ef43df5f5dab6839a4f23998d79ee68e902ff6d1
 
 @Component ({
     selector: 'pedido',
@@ -15,7 +12,6 @@ import { PedidoService } from '../pedido.service';
 })
 
 export class PedidoComponent implements OnInit {
-<<<<<<< HEAD
     constructor(private produtoService: ProdutoService, private pedidoService: PedidoService){}
 
     produtos: Produto[];
@@ -24,16 +20,7 @@ export class PedidoComponent implements OnInit {
     addLista(produto: Produto){
         this.pedidoService.addLista(produto);
     }
-=======
-    constructor(private produtoService: ProdutoService){}
-
-    produtos: Produto[];
-
-    atualizarProduto(produto: Produto){
-        this.produtoService.atualizar(produto);
-    }
-
->>>>>>> ef43df5f5dab6839a4f23998d79ee68e902ff6d1
+  
     ngOnInit(): void{
         this.produtoService.getProdutos()
          .then(produtos => this.produtos = produtos)
